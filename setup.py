03d22f77eb6b6a76968caf80102aa0315ff4c51b
An0nimia <41164945+An0nimia@users.noreply.github.com>
2020-10-14 11:55:43 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-	name = "acrcloud",
-	version = "2.0",
+	name = "python-acrcloud",
+	version = "1.0",
 	description = "Detects the songs from an audio",
 	license = "CC BY-NC-SA 4.0",
 	author = "An0nimia",
